d0be78331c23786a6b25f238583a39d320b4631e
Samuel Mateus <mateus.enc@gmail.com>
2021-03-02 13:51:07 -0300

    retirando o ' - ' que adicionava a cada tarefa

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -8,7 +8,7 @@ function botaoCriarTarefa(){
 
     const horario = document.getElementById("horario").value
     const diaSemana = document.getElementById("dias-semana").value
-    document.getElementById(diaSemana).innerHTML+=`<p onclick=cliqueiNaTarefa("p-id${qtdTarefa}") id=p-id${qtdTarefa} class=hora${horario}>-${tarefa}</p>`
+    document.getElementById(diaSemana).innerHTML+=`<p onclick=cliqueiNaTarefa("p-id${qtdTarefa}") id=p-id${qtdTarefa} class=hora${horario}>${tarefa}</p>`
     qtdTarefa++
   }
   else{
