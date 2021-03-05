6ed1ce4ddb27af929a58c860eb4e3e908d2347e8
Samuel Mateus <mateus.enc@gmail.com>
2021-03-05 12:56:58 -0300

    exercicio 12

--- semana4/lista-de-exercicios/exercicios.js
+++ semana4/lista-de-exercicios/exercicios.js
@@ -148,7 +148,13 @@ function ordenaArray(array) {
 // Exercício 12
 
 function filmeFavorito() {
-   // implemente sua lógica aqui
+   const objeto = {
+      nome : "O Diabo Veste Prada",
+      ano : 2006,
+      diretor : "David Frankel",
+      atores : ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"]
+   }
+   return objeto
 }
 
 // Exercício 13
