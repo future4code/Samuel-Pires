0e323e332ae5e6de355adf6500294787c012ece1
samenc99 <mateus.enc@gmail.com>
2021-04-12 14:57:46 -0300

    ListTripsPage 1.01

--- semana10/labex/src/pages/ListTripsPage.js
+++ semana10/labex/src/pages/ListTripsPage.js
@@ -3,18 +3,32 @@ import Header from '../components/Header'
 import {MiniLogo} from '../components/Logo'
 import Filter from '../components/Filter'
 import CardTrip from '../components/CardTrip'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  div{
+    width: min(900px, 90%);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`
 
 export default function ListTripsPage(){
   return(
     <Container>
-      <Header>
-        <MiniLogo></MiniLogo>
-        <button>Voltar</button>
-      </Header>
-      <Filter />
-      <CardContainer>
-        <CardTrip />
-      </CardContainer>
+      <div>
+        <Header>
+          <MiniLogo></MiniLogo>
+          <button>Voltar</button>
+        </Header>
+        <Filter />
+        <CardContainer>
+          <CardTrip />
+        </CardContainer>
+      </div>
     </Container>
   )
 }
