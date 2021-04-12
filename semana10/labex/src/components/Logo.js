81bdbbbc252d539283115b8c2605f9795d243545
samenc99 <mateus.enc@gmail.com>
2021-04-12 15:00:35 -0300

    MiniLogo 1.0

--- semana10/labex/src/components/Logo.js
+++ semana10/labex/src/components/Logo.js
@@ -1,15 +1,24 @@
 import styled from 'styled-components'
 
 const Logo = styled.div`
-width: 400px;
-height: 400px;
-display: flex;
-align-items: center;
-justify-content: center;
-  img{
-    width: 100%;
-    max-height: 100%;
-  }
+  width: 400px;
+  height: 400px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+    img{
+      width: 100%;
+      max-height: 100%;
+    }
+`
+
+export const MiniLogo = styled.div`
+  width: 60px;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 export default Logo
+
