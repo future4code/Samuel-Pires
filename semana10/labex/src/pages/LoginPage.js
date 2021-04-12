3c07563297f3b6fa7b12a1586a5cc3d6c62ad360
samenc99 <mateus.enc@gmail.com>
2021-04-12 14:18:13 -0300

    LoginPage 1.0

---
+++ semana10/labex/src/pages/LoginPage.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+import Logo from '../components/Logo'
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const DivInputs = styled.div`
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  button{
+    border: 1px solid black;
+    border-radius: 20px;
+  }
+`
+
+export default function(){
+  return(
+    <Container>
+      <Logo>
+        {/* aqui logo */}
+      </Logo>
+      <DivInputs>
+        <input placeholder='Email' />
+        <input placeholder='Senha' type='password' />
+      </DivInputs>
+    </Container>
+  )
+}
