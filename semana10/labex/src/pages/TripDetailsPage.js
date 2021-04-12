637df2672bfc4e8ee98bc74a3208dcbe4be21fea
samenc99 <mateus.enc@gmail.com>
2021-04-12 15:50:22 -0300

    TripDetails 1.0

---
+++ semana10/labex/src/pages/TripDetailsPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CardCandidate from '../components/CardCandidate'
+import CardTrip from '../components/CardTrip'
+import Container from '../components/Container'
+
+export default function TripDetailsPage(){
+  return(
+    <Container>
+      <CardTrip />
+      <CardCandidate />
+    </Container>
+  )
+}
