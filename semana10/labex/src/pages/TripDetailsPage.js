62a1e855568e659f8ec774b52a45bc465051d170
samenc99 <mateus.enc@gmail.com>
2021-04-14 14:26:38 -0300

    requisição api para trip details

--- semana10/labex/src/pages/TripDetailsPage.js
+++ semana10/labex/src/pages/TripDetailsPage.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router'
+import { useState } from 'react/cjs/react.development'
 import CardCandidate from '../components/CardCandidate'
 import CardTrip from '../components/CardTrip'
 import Container from '../components/Container'
 import useProtectedPage from '../hooks/useProtectedPage'
+import { baseUrl } from '../parameters'
 
 export default function TripDetailsPage(){
+  const {id} = useParams()
+  const [trip, setTrip] = useState({})
+
+  const getTripDetail = async()=>{
+    const token = window.localStorage.getItem('token')
+    try{
+      const res = await axios.get(`${baseUrl}/trip/${id}`,{
+        headers:{
+          auth: token
+        }
+      })
+      setTrip(res.data.trip)
+      console.log('Trip', trip)
+    }
+    catch(err){
+      console.log('erro ao pegar viagem específica', err)
+    }
+  }
   useProtectedPage()
+  useEffect(()=>{
+    getTripDetail()
+  },[])
+
   return(
     <Container>
       {/* <CardTrip />
