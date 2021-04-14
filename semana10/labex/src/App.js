f44a6bc390afca508d7c30c06551fb9bba5e918f
samenc99 <mateus.enc@gmail.com>
2021-04-13 21:45:42 -0300

    rotas adicionadas

--- semana10/labex/src/App.js
+++ semana10/labex/src/App.js
@@ -1,8 +1,42 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+import HomePage from './pages/HomePage'
+import ListTripsPage from './pages/ListTripsPage'
+import AdminHomePage from './pages/AdminHomePage'
+import ApplicationFormPage from './pages/ApplicationFormPage'
+import CreateTripPage from './pages/CreateTripPage'
+import LoginPage from './pages/LoginPage'
+import TripDetailsPage from './pages/TripDetailsPage'
+
 
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/trips/list">
+          <ListTripsPage />
+        </Route>
+        <Route exact path="/trips/application">
+          <ApplicationFormPage />
+        </Route>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/admin/trips/list">
+          <AdminHomePage />
+        </Route>
+        <Route exact path="/admin/trips/create">
+          <CreateTripPage />
+        </Route>
+        <Route exact path="/admin/trips/:id">
+          <TripDetailsPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
