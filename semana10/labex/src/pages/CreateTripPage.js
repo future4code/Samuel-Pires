768eca688bbbac6cd1dd8ba719e09c81be5a88d1
samenc99 <mateus.enc@gmail.com>
2021-04-14 14:34:09 -0300

    requisição api para criar  viagem

--- semana10/labex/src/pages/CreateTripPage.js
+++ semana10/labex/src/pages/CreateTripPage.js
@@ -3,6 +3,9 @@ import styled from 'styled-components'
 import Header from '../components/Header'
 import Container from '../components/Container'
 import useProtectedPage from '../hooks/useProtectedPage'
+import { useInput } from '../hooks/useInput'
+import axios from 'axios'
+import { baseUrl, headers } from '../parameters'
 
 const InputA = styled.input``
 const InputB = styled.input`` 
@@ -12,6 +15,27 @@ const Button = styled.button``
 
 export default function CreateTripPage(){
   useProtectedPage()
+
+  const[name, setName] = useInput()
+  const[planet, setPlanet] = useInput()
+  const[date, setDate] = useInput()
+  const[description, setDescription] = useInput()
+  const[durationInDays, setDurationInDays] = useInput()
+
+  const createTrip = async()=>{
+    const token = window.localStorage.getItem('token')
+    const body={
+      name, planet, date, description, durationInDays
+    }
+    try{
+      await axios.post(`${baseUrl}/trips`, body, headers(token))
+      console.log('sucesso ao criar viagem')
+    }
+    catch(err){
+      console.log('erro ao criar viagem', err)
+    }
+  }
+
   return(
     <Container>
       {/* <Header>
