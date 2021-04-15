c3e6cdd384c581fca5d458ef633b007a3be08308
samenc99 <mateus.enc@gmail.com>
2021-04-15 19:02:02 -0300

    CreateTripPage 2.0

--- semana10/labex/src/pages/CreateTripPage.js
+++ semana10/labex/src/pages/CreateTripPage.js
@@ -1,57 +1,140 @@
-import React from 'react'
-import styled from 'styled-components'
-import Header from '../components/Header'
-import {ContainerStyled} from '../styledComponents'
-import useProtectedPage from '../hooks/useProtectedPage'
-import { useInput } from '../hooks/useInput'
-import axios from 'axios'
-import { baseUrl, headers } from '../parameters'
+import React from "react";
+import styled from "styled-components";
+import Header from "../components/Header";
+import { ContainerStyled, ButtonStyled} from "../styledComponents";
+import useProtectedPage from "../hooks/useProtectedPage";
+import { useInput } from "../hooks/useInput";
+import axios from "axios";
+import { baseUrl, headers } from "../parameters";
+import useForm from "../hooks/useForm";
 
-const InputA = styled.input``
-const InputB = styled.input`` 
-const Planet = styled.select``
-const Text = styled.textarea``
-const Button = styled.button``
+const InputA = styled.input`
+  width: 100%;
+`;
+const InputB = styled.input`
+  width: 48%;
+`;
+const Planet = styled.select`
+  width: 100%;
+`;
+const Text = styled.textarea`
+  width: 100%;
+  height: 300px;
+`;
+const Button = styled(ButtonStyled)`
+  width: 100%;
+  margin-top: 10px;
+`;
+const Form = styled.form`
+  width: 90%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+const Container = styled(ContainerStyled)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-export default function CreateTripPage(){
-  useProtectedPage()
+const initialForm = {
+  name: "",
+  planet: "",
+  date: "",
+  description: "",
+  durationInDays: "",
+};
 
-  const[name, setName] = useInput()
-  const[planet, setPlanet] = useInput()
-  const[date, setDate] = useInput()
-  const[description, setDescription] = useInput()
-  const[durationInDays, setDurationInDays] = useInput()
+export default function CreateTripPage() {
+  useProtectedPage();
+  const [form, setForm] = useForm(initialForm);
+  const data = new Date()
 
-  const createTrip = async()=>{
-    const token = window.localStorage.getItem('token')
-    const body={
-      name, planet, date, description, durationInDays
-    }
-    try{
-      await axios.post(`${baseUrl}/trips`, body, headers(token))
-      console.log('sucesso ao criar viagem')
-    }
-    catch(err){
-      console.log('erro ao criar viagem', err)
-    }
+  const dataString = (data)=>{
+    console.log('data', data)
+    const year = data.getFullYear().toString()
+
+    let month
+    if(data.getMonth()+1<10)month='0'+(data.getMonth()+1).toString()
+    else month = (data.getMonth()+1).toString()
+
+    let day
+    if((data.getDate()+1)<10)day='0'+(data.getDate()+1).toString()
+    else day = (data.getDate()+1).toString()
+    
+    return year+'-'+month+'-'+day
   }
 
-  return(
-    <ContainerStyled>
-      {/* <Header>
+  const createTrip = async (e) => {
+    e.preventDefault()
+    const token = window.localStorage.getItem("token");
 
-      </Header>
-      <div>
-        <InputA placeholder='Nome...'/>
-        <InputB placeholder='Data...' type='date' />
-        <InputB placeholder='Duração em dias...' type='number' />
-        <Planet>
-          <option></option>
+    try {
+      await axios.post(`${baseUrl}/trips`, form, headers(token));
+      console.log("sucesso ao criar viagem");
+      setForm(initialForm)
+    } catch (err) {
+      console.log("erro ao criar viagem", err);
+    }
+  };
+  
+
+
+  console.log('form  data', form.date)
+  console.log('dataString', dataString(new Date()))
+  return (
+    <Container>
+      <Header />
+      <Form onSubmit={createTrip}>
+        <InputA
+          name="name"
+          required
+          minLength="5"
+          placeholder="Nome..."
+          value={form.name}
+          onChange={setForm}
+        />
+        <InputB
+          name="date"
+          required
+          type="date"
+          min={dataString(new Date())}
+          title='Requer uma data no futuro'
+          value={form.date}
+          onChange={setForm}
+        />
+        <InputB
+          name="durationInDays"
+          required
+          type="number"
+          min="50"
+          placeholder="Duração em dias..."
+          value={form.durationInDays}
+          onChange={setForm}
+        />
+        <Planet name="planet" required value={form.planet} onChange={setForm}>
+          <option value=""></option>
+          <option value="Mercury">Mercury</option>
+          <option value="Venus">Venus</option>
+          <option value="Earth">Earth</option>
+          <option value="Mars">Mars</option>
+          <option value="Jupiter">Jupiter</option>
+          <option value="Mercury">Mercury</option>
+          <option value="Saturn">Mercury</option>
+          <option value="Uranus">Uranus</option>
+          <option value="Neptune">Neptune</option>
+          <option value="Pluto">Pluto</option>
         </Planet>
-        <Text />
+        <Text
+          name="description"
+          required
+          minLength="30"
+          placeholder="Descrição..."
+          value={form.description}
+          onChange={setForm}
+        />
         <Button>Criar</Button>
-      </div> */}
-      <p>CreateTripPage</p>
-    </ContainerStyled>
-  )
+      </Form>
+    </Container>
+  );
 }
