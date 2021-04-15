791719ca29a5bfb22b6702a9556b6070f5b3471d
samenc99 <mateus.enc@gmail.com>
2021-04-15 19:55:42 -0300

    AdminHomePage listar viagens e poder excluir

--- semana10/labex/src/pages/AdminHomePage.js
+++ semana10/labex/src/pages/AdminHomePage.js
@@ -1,17 +1,48 @@
 import styled from "styled-components";
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Header from '../components/Header'
 import {ContainerStyled} from '../styledComponents'
 import useProtectedPage from "../hooks/useProtectedPage";
 import axios from "axios";
 import { baseUrl, headers } from "../parameters";
 import { useParams } from "react-router-dom";
+import CardTrip from "../components/CardTrip";
+
+const Container = styled(ContainerStyled)`
+  display:flex;
+  flex-direction: column;
+`
+
+const Cards = styled.div`
+  width: 100%;
+  overflow: auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 5px;
+`
 
 export default function AdminHomePage(){
   useProtectedPage()
-  const {id} = useParams()
 
-  const deleteTrip = async()=>{
+  const [trips, setTrips] = useState([])
+
+  const getTrips = async()=>{
+    try{
+      const res = await axios.get(`${baseUrl}/trips`)
+      setTrips(res.data.trips)
+    }
+    catch(err){
+      console.log('erro ao obter viagens getTrips', err)
+    }
+  }
+
+  useEffect(()=>{
+    getTrips()
+  },[])
+
+
+  const deleteTrip = async(id)=>{
     const token = window.localStorage.getItem('token')
     try{
       const res = await axios.delete(`${baseUrl}/trips/${id}`, headers(token))
@@ -21,15 +52,18 @@ export default function AdminHomePage(){
       console.log('erro ao excluir resposta', err)
     }
   }
+
+  const tripsRendered = ()=>{
+    return trips.map((trip)=>{
+      return <CardTrip trip={trip} del={true} deleteTrip={deleteTrip}/>
+    })
+  }
   return(
-    <ContainerStyled>
-      {/* <Header></Header>
-      <div>
-        <CardTripContainer>
-          <CardTripAdmin />
-        </CardTripContainer>
-      </div> */}
-      <p>AdminHomePage</p>
-    </ContainerStyled>
+    <Container>
+      <Header />
+      <Cards>
+        {tripsRendered()}
+      </Cards>
+    </Container>
   )
 }
