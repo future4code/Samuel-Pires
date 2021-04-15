6209bd2f439596918c457600d23f034ebb13d742
samenc99 <mateus.enc@gmail.com>
2021-04-14 23:41:38 -0300

    adicionando botão de excluir viagem no CardTrip que só ficará disponível para admin

--- semana10/labex/src/components/CardTrip.js
+++ semana10/labex/src/components/CardTrip.js
@@ -1,19 +1,28 @@
 import styled from "styled-components"
 import React from 'react'
 
-const Container = styled.div`
+const ContainerAll = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 49%;
   margin-bottom: 10px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
   height: 300px;
+  max-height: 500px;
   box-shadow: 1px 1px 3px #003759;
   :hover{
     box-shadow: 1px 1px 10px #002134;
   }
   overflow: auto;
+`
+
+const Container = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  
 `
 
 const P = styled.div`
@@ -25,16 +34,37 @@ const P = styled.div`
   }
 `
 
+const Svg = styled.svg`
+  width: 50px;
+  padding: 10px;
+  margin-right: 10px;
+  fill: currentColor;
+  color: #005288;
+  transition: all 0.3s;
+  :hover{
+    border-radius: 25px;
+    background-color: #a7a9ac;
+    color: #002134;
+    transform: scale(1.3);
+  }
+`
+
 export default function CardTrip(props){
   const {id, name, description, planet, duration, date} = props.trip
   return(
-    <Container key={id}>
-      <P>Nome <p>{name}</p></P>
-      <P>Planeta <p>{planet}</p></P>
-      <P>Duração <p>{duration}</p></P>
-      <P>Data <p>{date}</p></P>
-      <P>Descrição <p>{description}</p></P>
-      
-    </Container>
+    <ContainerAll>
+      <Container key={id}>
+        <P>Nome <p>{name}</p></P>
+        <P>Planeta <p>{planet}</p></P>
+        <P>Duração <p>{duration}</p></P>
+        <P>Data <p>{date}</p></P>
+        <P>Descrição <p>{description}</p></P>
+      </Container>
+      {props.del &&
+        <Svg focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+          <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path>
+        </Svg>
+      }
+    </ContainerAll>
   )
 }
