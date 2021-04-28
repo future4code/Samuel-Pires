5ad5f575717f59cfd9ad7c6e59e452f8af7c06f7
samenc99 <mateus.enc@gmail.com>
2021-04-28 11:40:23 -0300

    Post 0.0

---
+++ semana12/labeddit/src/pages/Private/Post.jsx
@@ -0,0 +1,56 @@
+import React,{useState, useEffect} from 'react'
+import {useParams, useHistory} from 'react-router-dom'
+import {
+  All,
+  Container
+} from './styled'
+import {useGetApi} from "../../hooks/useRequest";
+import config from "../config";
+import Loading from "./components/Loading/Loading";
+import ContentCard from "./components/ContentCard/ContentCard";
+
+export default function Post(props){
+  const [idPost, setId] = useState('')
+  const {id} = useParams()
+  const history = useHistory()
+  const [post, getPostApi] = useGetApi({})
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    if(id){
+      setId(id)
+    }
+    else if(props.id){
+      setId(props.id)
+    }
+  },[id,props])
+
+  useEffect(()=>{
+    if(idPost){
+      getPostApi(`/posts/${idPost}`, config(), (res, setValue)=>{
+        if(res.data.post.text){
+          setValue(res.data.post)
+          setLoading(false)
+        }
+        else{
+          //fiz q requisição e peguei o post, agora tenho que continuar
+        }
+
+      },(err)=>{
+        alert('erro aqui', err)
+      })
+    }
+  },[idPost])
+
+
+
+  return(
+    <All>
+      {loading? (
+        <Loading />
+      ):(
+        <ContentCard value={post} />
+      )}
+    </All>
+  )
+}
--- semana12/labeddit/src/routes/Routes.jsx
+++ semana12/labeddit/src/routes/Routes.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import SignIn from '../pages/Singn/SignIn'
 import SignUp from '../pages/Singn/SignUp'
 import Feed from "../pages/Private/Feed";
-
+import Post from "../pages/Private/Post";
 export default function (){
   return(
     <BrowserRouter>
@@ -14,8 +14,14 @@ export default function (){
         <Route exact path={'/signup'}>
           <SignUp />
         </Route>
+        <Route exact path={'/post/:id'}>
+          <Post />
+        </Route>
         <Route exact path={'/'}>
           <Feed />
+        </Route>
+        <Route>
+          <div>Página não encontrada</div>
         </Route>
       </Switch>
     </BrowserRouter>
