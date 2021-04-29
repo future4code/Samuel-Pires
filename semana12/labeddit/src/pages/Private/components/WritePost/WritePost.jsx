503661fb1372bf8a413f3b6ee91e8a3fba49bfb0
samenc99 <mateus.enc@gmail.com>
2021-04-29 17:24:27 -0300

    WritePost funcionando - falta estilização

--- semana12/labeddit/src/pages/Private/components/WritePost/WritePost.jsx
+++ semana12/labeddit/src/pages/Private/components/WritePost/WritePost.jsx
@@ -3,13 +3,24 @@ import {
   Form, Title, Text, Button
 } from "./styled";
 import {MySendIcon} from "./styledMaterial";
+import useForm from "../../../../hooks/useForm";
+import {postApi} from "../../../../hooks/useRequest";
+import config from "../../../config";
 
 export default function WritePost(){
+  const [form, setForm] = useForm({title:'',text:''})
+
+  const onSubmitForm=(e)=>{
+    e.preventDefault()
+    postApi('/posts',form, config(),(res)=>{
+      window.alert('Seu post foi enviado com sucesso')
+    })
+  }
 
   return(
-    <Form>
-      <Title placeholder={'Título...'}/>
-      <Text placeholder={'Escreva seu post...'}/>
+    <Form onSubmit={onSubmitForm}>
+      <Title placeholder={'Título...'} required min={'3'} name={'title'} value={form.title} onChange={setForm}/>
+      <Text placeholder={'Escreva seu post...'} required min={'3'} name={'text'} value={form.text} onChange={setForm}/>
       <Button><MySendIcon/></Button>
     </Form>
   )
