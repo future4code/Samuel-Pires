c9842cc020a87272f3af7ee70be57107e3186955
samenc99 <mateus.enc@gmail.com>
2021-05-17 11:30:37 -0300

    exercicios transferido para pastas separadas

--- semana15/aula41/exercicio3/index.js
+++ semana15/aula41/exercicio3/index.js
@@ -1,5 +1,10 @@
 const list = ['projeto labenu', 'instalar app']
 
+if(process.argv.length<3){
+  console.log('Esperava receber pelo menos 1 argumento')
+  process.exit()
+}
+
 for(let i=2; i<process.argv.length; i++){
   list.push(process.argv[i])
 }
