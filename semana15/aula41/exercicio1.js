f7a3442db227309861556e076e05f5fefbafae26
samenc99 <mateus.enc@gmail.com>
2021-05-17 10:41:54 -0300

    exercicio 1

---
+++ semana15/aula41/exercicio1.js
@@ -0,0 +1,7 @@
+//a) Através do process.argv[posicao_parametro]
+
+//
+console.log(`Olá, ${process.argv[2]}! Você tem ${process.argv[3]} anos.`)
+
+console.log(`Olá, ${process.argv[2]}! Você tem ${process.argv[3]} anos. Em sete 
+anos você terá ${Number(process.argv[3])+7}.`)
