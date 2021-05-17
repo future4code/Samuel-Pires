26a8158bebe1633e6faa023f5d4ab34fce81a676
samenc99 <mateus.enc@gmail.com>
2021-05-17 12:11:38 -0300

    persistencia de dados da lista de tarefas exercício 3

--- semana15/aula41/exercicio3/index.js
+++ semana15/aula41/exercicio3/index.js
@@ -1,12 +1,17 @@
-const list = ['projeto labenu', 'instalar app']
-
 if(process.argv.length<3){
   console.log('Esperava receber pelo menos 1 argumento')
   process.exit()
 }
 
+let fs = require('fs')
+const list = JSON.parse(fs.readFileSync('lista.txt').toString())
+
 for(let i=2; i<process.argv.length; i++){
   list.push(process.argv[i])
 }
 
-console.log('tarefas:', list)
+console.log('list', list)
+
+fs.writeFile('lista.txt', JSON.stringify(list), function (err, file){
+  if(err) throw err
+})
