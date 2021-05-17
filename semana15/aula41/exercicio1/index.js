7857bdb7008d8e42750f3b0d439adacb2900821d
samenc99 <mateus.enc@gmail.com>
2021-05-17 13:41:32 -0300

    retirando enter do console log

--- semana15/aula41/exercicio1/index.js
+++ semana15/aula41/exercicio1/index.js
@@ -9,5 +9,4 @@ if(process.argv.length!==4){
 console.log(`Olá, ${process.argv[2]}! Você tem ${process.argv[3]} anos.`)
 
 //c
-console.log(`Olá, ${process.argv[2]}! Você tem ${process.argv[3]} anos. Em sete 
-anos você terá ${Number(process.argv[3])+7}.`)
+console.log(`Olá, ${process.argv[2]}! Você tem ${process.argv[3]} anos. Em sete anos você terá ${Number(process.argv[3])+7}.`)
