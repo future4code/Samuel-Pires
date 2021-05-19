c90a3feb25e7ad48a417ccd7740943440170f75f
samenc99 <mateus.enc@gmail.com>
2021-05-19 15:54:28 -0300

    endpoint 1

--- semana15/aula43/src/index.ts
+++ semana15/aula43/src/index.ts
@@ -1,12 +1,15 @@
 import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
+import {countries} from "./countries";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('')
+app.get('/countries/all', (req: Request, res:Response)=>{
+  res.status(200).send(countries)
+})
 
 
 
