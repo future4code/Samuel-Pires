ad5e62a2be0569d6bd98ec3f4549d642fd2df780
samenc99 <mateus.enc@gmail.com>
2021-05-19 20:04:04 -0300

    endpoint 3

--- semana15/aula43/src/index.ts
+++ semana15/aula43/src/index.ts
@@ -1,6 +1,6 @@
 import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
-import {countries, country} from "./countries";
+import {countries, country, CONTINENTS} from "./countries";
 
 const app: Express = express();
 
@@ -9,6 +9,41 @@ app.use(cors());
 
 app.get('/countries/all', (req: Request, res:Response)=>{
   res.status(200).send(countries)
+})
+
+app.get('/countries/search', (req: Request, res: Response) => {
+  const includes = (str1:string, str2:string) : boolean =>{
+    return str1.toLowerCase().includes(str2.toLowerCase())
+  }
+
+  try{
+    const name = req.query.name as string
+    const capital = req.query.capital as string
+    const continent = req.query.continent as string
+
+    if(!name && !capital && !continent){
+      throw new Error('Por favor, passe os parâmetros corretamente.')
+    }
+
+    const result : country[] = countries.filter(c=>{
+      if(name){
+        if(!includes(c.name, name))return false
+      }
+      if(capital){
+        if(!includes(c.capital, capital))return false
+      }
+      if(continent){
+        if(!includes(c.continent, continent))return false
+      }
+      return true
+    })
+    if(result.length===0){
+      throw new Error('Nenhum resultado correspondente')
+    }
+    res.send(result)
+  }catch (err){
+    res.status(400).send({message: err.message})
+  }
 })
 
 app.get('/countries/:id',(req: Request, res: Response)=>{
