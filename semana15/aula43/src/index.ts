445f3be79b88bc3387568f2890dce697e264d5c9
samenc99 <mateus.enc@gmail.com>
2021-05-19 19:15:53 -0300

    endpoint 2

--- semana15/aula43/src/index.ts
+++ semana15/aula43/src/index.ts
@@ -1,6 +1,6 @@
 import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
-import {countries} from "./countries";
+import {countries, country} from "./countries";
 
 const app: Express = express();
 
@@ -11,7 +11,24 @@ app.get('/countries/all', (req: Request, res:Response)=>{
   res.status(200).send(countries)
 })
 
+app.get('/countries/:id',(req: Request, res: Response)=>{
+ try{
+   const {id} = req.params
+   if(isNaN(Number(id))){
+     throw new Error('Id precisa ser um número')
+   }
 
+   const result : country | undefined = countries.find(
+     c => c.id === Number(id)
+   )
+   if(!result){
+     throw new Error('Não encontrado')
+   }
+   res.status(200).send(result)
+ }catch (err){
+   res.status(400).send({message: err.message})
+ }
+})
 
 
 app.listen(3003, ()=>{
