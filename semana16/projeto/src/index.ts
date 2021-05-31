edf2910d977514426e935c1073fa5e039cbc404d
samenc99 <mateus.enc@gmail.com>
2021-05-31 15:11:05 -0300

    endpoint 6 - pegar todos usuários

--- semana16/projeto/src/index.ts
+++ semana16/projeto/src/index.ts
@@ -14,6 +14,15 @@ app.get('/ping', (req: Request, res: Response) => {
   }
 })
 
+app.get('/user/all', async(req: Request, res: Response) => {
+  try {
+    const users = await connection('Users_projeto16').select('id','nickname')
+    res.status(200).send({users})
+  } catch (err) {
+    res.status(400).send({message: err.message})
+  }
+})
+
 app.get('/user/:id', async(req: Request, res: Response) => {
   try {
     const {id} = req.params
