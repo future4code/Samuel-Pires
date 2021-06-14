06f8c7e8b29f160ec3533e4db12bb512e49cb960
samenc99 <mateus.enc@gmail.com>
2021-06-14 15:51:02 -0300

    exercício 1 - retornando token

--- semana19/aula55/src/controller/post/signup.ts
+++ semana19/aula55/src/controller/post/signup.ts
@@ -5,8 +5,8 @@ import signupBussines from "../../bussiness/signupBussines";
 export default async function (req: Request, res: Response): Promise<any> {
   try {
     const {name, email, password, role} = req.body
-    await signupBussines({name, email, password, role})
-    res.status(201).send({message: 'Created!'})
+    const token = await signupBussines({name, email, password, role})
+    res.status(201).send({token})
   } catch (err) {
     if(err.sqlMessage && err.sqlMessage.includes('Duplicate entry')){
       err.message = 'E-mail already exists.'
