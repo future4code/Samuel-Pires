b64e757bd97afcdf41b6a837bbad52ce772c82b6
samenc99 <mateus.enc@gmail.com>
2021-06-22 14:07:30 -0300

    exercício 2

--- semana20/aula60/src/validateCharacter.ts
+++ semana20/aula60/src/validateCharacter.ts
@@ -1,7 +1,7 @@
 import {Character} from "../model/interfaces";
 
 export const validateCharacter = (character : Character) : boolean=>{
-  return character.name &&
+  return !!character.name &&
     character.life>0 &&
     character.defense>0 &&
     character.strength>0
